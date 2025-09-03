77dd4e60870429af558714666091921a033f9a89
XJay18 <jaycao2008@126.com>
2025-09-03 20:54:50 +0000

    fixed path

--- examples/coupling/rod_on_rigid.py
+++ examples/coupling/rod_on_rigid.py
@@ -9,7 +9,7 @@ def test_v1(scene):
             segment_radius=0.005,
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/test.npy",
+            file="test.npy",
             scale=1.0,
             pos=(0.5, 0.5, 0.3),
             euler=(0.0, 0.0, 15.0),
@@ -26,7 +26,7 @@ def test_v1(scene):
             segment_radius=0.005,
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/test.npy",
+            file="test.npy",
             scale=1.0,
             pos=(0.55, 0.43, 0.4),
             euler=(0.0, 0.0, 0.0),
@@ -43,7 +43,7 @@ def test_v1(scene):
             segment_radius=0.02,
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/fixed.npy",
+            file="fixed.npy",
             scale=1.0,
             pos=(0.75, 0.435, 0.2),
             euler=(0.0, 0.0, -75.0),
@@ -60,7 +60,7 @@ def test_v1(scene):
             segment_radius=0.02,
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/fixed.npy",
+            file="fixed.npy",
             scale=1.0,
             pos=(1.05, 0.435, 0.25),
             euler=(0.0, 0.0, -75.0),
@@ -95,7 +95,7 @@ def test_v2(scene):
             segment_radius=0.005,
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/test.npy",
+            file="test.npy",
             scale=1.0,
             pos=(0.7, 0.05, 0.5),
             euler=(0.0, 0.0, 15.0),
@@ -114,7 +114,7 @@ def test_v2(scene):
             kinetic_friction=1.25
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/test.npy",
+            file="test.npy",
             scale=1.0,
             pos=(0.7, 0.15, 0.5),
             euler=(0.0, 0.0, 15.0),
@@ -131,7 +131,7 @@ def test_v2(scene):
             segment_radius=0.02,
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/fixed.npy",
+            file="fixed.npy",
             scale=1.0,
             pos=(0.85, 0.05, 0.3),
             euler=(0.0, 0.0, 105.0),
@@ -148,7 +148,7 @@ def test_v2(scene):
             segment_radius=0.02,
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/fixed.npy",
+            file="fixed.npy",
             scale=1.0,
             pos=(1.25, 0.15, 0.3),
             euler=(0.0, 0.0, 105.0),
@@ -179,7 +179,7 @@ def test_v3(scene):
             G=1e7
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/circle.npy",
+            file="circle.npy",
             scale=1.0,
             pos=(1.02, -0.03, 0.15),
             euler=(0.0, 90.0, 105.0),
@@ -198,7 +198,7 @@ def test_v3(scene):
             G=1e7
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/testshort.npy",
+            file="testshort.npy",
             scale=1.0,
             pos=(0.95, 0.0, 0.1),
             euler=(0.0, 0.0, 15.0),
@@ -215,7 +215,7 @@ def test_v3(scene):
             segment_radius=0.01,
         ),
         morph=gs.morphs.Rod(
-            file="/home/junyicao_umass_edu/research/genesis_project/fixed.npy",
+            file="fixed.npy",
             scale=1.0,
             pos=(0.9, 0.0, 0.05),
             euler=(0.0, 0.0, 105.0),
