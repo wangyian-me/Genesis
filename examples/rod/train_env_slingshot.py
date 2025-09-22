636b4a6250b31752f90be24137ef75e208c3616a
XJay18 <jaycao2008@126.com>
2025-09-22 22:15:19 +0000

    fix env slingshot

--- examples/rod/train_env_slingshot.py
+++ examples/rod/train_env_slingshot.py
@@ -241,11 +241,11 @@ class Train_Env_Slingshot(Train_Env):
                     ever_nan[newly_nan_after] = True
                     alive[newly_nan_after] = False
 
-            fixed_np = np.zeros((self.n_envs, self.rope.n_vertices), dtype=bool)
-            self.rope.set_fixed(0, fixed_np)    # release all
+        fixed_np = np.zeros((self.n_envs, self.rope.n_vertices), dtype=bool)
+        self.rope.set_fixed(0, fixed_np)    # release all
 
-            for _ in range(500):
-                self.scene.step()
+        for _ in range(500):
+            self.scene.step()
 
         # Compute base rewards
         env_rewards = np.asarray(self.reward(), dtype=np.float32)
