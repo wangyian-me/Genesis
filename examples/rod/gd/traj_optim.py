8100ac8d93863ab858eef141237dc46ee1ac9774
Junyi Cao <junyicao@cs.umass.edu>
2025-09-23 08:10:38 -0400

    fix loss scale

--- examples/rod/gd/traj_optim.py
+++ examples/rod/gd/traj_optim.py
@@ -19,7 +19,8 @@ def create_custom_array(N):
     base_seq = torch.ones(N, dtype=gs.tc_float)
     base_seq[:N-1] = 0.5 / (N - 1)
     base_seq[N-1] = 0.5
-    assert base_seq.sum() == 1.0
+    # assert base_seq.sum() == 1.0
+    base_seq = base_seq / base_seq.sum() # ensure sum to 1
     return base_seq
 
 class TrajOptim:
