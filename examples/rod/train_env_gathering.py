e030f8738a17e97fa11e93866c4d90f5072b9aba
XJay18 <jaycao2008@126.com>
2025-11-08 06:19:10 +0000

    update gathering env

--- examples/rod/train_env_gathering.py
+++ examples/rod/train_env_gathering.py
@@ -5,7 +5,34 @@ from train_env import Train_Env
 
 class Train_Env_Gathering(Train_Env):
     def __init__(self, task='wiring', GUI=False, camera=False, log_dir="xxx/wiring", n_envs=5, requires_grad=False):
-        super().__init__(task, GUI=GUI, camera=camera, n_envs=n_envs, log_dir=log_dir, requires_grad=requires_grad)
+        gs.init(seed=0, precision="64", logging_level="error", backend=gs.gpu, performance_mode=True)
+        viewer_options = gs.options.ViewerOptions(
+            camera_pos=(3, -1, 1.5),
+            camera_lookat=(0.0, 0.0, 0.0),
+            camera_fov=30,
+            max_FPS=60,
+        )
+
+        scene = gs.Scene(
+            viewer_options=viewer_options,
+            sim_options=gs.options.SimOptions(
+                dt=1e-3,
+                substeps=5,
+                requires_grad=requires_grad,
+            ),
+            mpm_options=gs.options.MPMOptions(
+                lower_bound=(-0.2, -0.5, -0.1),
+                upper_bound=(0.8, 0.5, 0.9),
+                grid_density=100,
+            ),
+            rod_options=gs.options.RodOptions(
+                damping=15.0,
+                angular_damping=10.0,
+                n_pbd_iters=20,
+            ),
+            show_viewer=GUI,
+        )
+        super().__init__(task, scene=scene, GUI=GUI, camera=camera, n_envs=n_envs, log_dir=log_dir, requires_grad=requires_grad)
         self.steps_interval = 200
 
     def construct_scene(self, camera):
@@ -20,7 +47,7 @@ class Train_Env_Gathering(Train_Env):
         self.rope = self.scene.add_entity(
             material=gs.materials.ROD.Base(
                 segment_radius=segment_radius,
-                segment_mass=0.001,
+                segment_mass=0.01,
                 K=1e5,
                 E=1e3,
                 G=1e3,
@@ -34,19 +61,24 @@ class Train_Env_Gathering(Train_Env):
                 pos=(-0.15, 0.1, 0.02),
                 euler=(0, 0, 0),
             ),
-            surface=gs.surfaces.Default(
-                color=(0.4, 1.0, 0.4),
+            surface=gs.surfaces.Rough(
+                diffuse_texture=gs.textures.ImageTexture(
+                    image_path="textures/rope01.png",
+                ),
                 vis_mode='recon',
+                normal_diff_clamp=1,
             )
         )
 
         self.sphere = self.scene.add_entity(
-            material=gs.materials.Rigid(
-                needs_coup=True, coup_friction=0.3,
+            material=gs.materials.MPM.ElastoPlastic(
+                E=1e5,
+                nu=0.3,
+                von_mises_yield_stress=1e3,
             ),
             morph=gs.morphs.Sphere(
                 radius=0.05,
-                pos=(0.3, -0.07, 0.05),
+                pos=(0.25, 0.02, 0.07),
                 euler=(0, 0, 0),
             ),
             surface=gs.surfaces.Default(
@@ -60,8 +92,8 @@ class Train_Env_Gathering(Train_Env):
             ),
             morph=gs.morphs.Mesh(
                 file="meshes/bunny.obj",
-                scale=0.2,
-                pos=(0.6, -0.3, 0.1),
+                scale=0.1,
+                pos=(0.5, -0.05, 0.07),
             ),
             surface=gs.surfaces.Default(
                 color=(0., 0.42, 0.47)
@@ -73,9 +105,9 @@ class Train_Env_Gathering(Train_Env):
                 needs_coup=True, coup_friction=0.3,
             ),
             morph=gs.morphs.Cylinder(
-                radius=0.08,
+                radius=0.04,
                 height=0.12,
-                pos=(0.2, -0.4, 0.06),
+                pos=(0.08, -0.08, 0.1),
                 euler=(0, 0, 0),
             ),
             surface=gs.surfaces.Default(
@@ -94,20 +126,23 @@ class Train_Env_Gathering(Train_Env):
     def construct_cameras(self):
         cameras = list()
         cameras.append(self.scene.add_camera(
-            res=(1200, 900), pos=(3, -1, 1.5), up=(0, 0, 1),
-            lookat=(0.3, -0.1, 0), fov=30, GUI=False
+            res=(1200, 900), pos=(0.4, 2., 0.7), up=(0, 0, 1),
+            lookat=(0.25, 0., 0), fov=30, GUI=False
         ))
         cameras.append(self.scene.add_camera(
-            res=(1200, 900), pos=(-1.5, -1, 1.4), up=(0, 0, 1),
-            lookat=(0.3, -0.1, 0.), fov=30, GUI=False
+            res=(1200, 900), pos=(1.5, 0.75, 1.5), up=(0, 0, 1),
+            lookat=(0.25, -0.1, 0.), fov=30, GUI=False
         ))
 
         self.cameras = cameras
 
     def reward(self):
 
+        verts_batch = self.rope.get_all_verts_tc()  # shape: (n_envs, n_vertices, 3)
 
-        pos1 = self.sphere.get_pos()    # shape: (n_envs, 3)
+        pos1 = self.sphere.get_particles()    # shape: (n_envs, n_particles, 3)
+        pos1 = np.mean(pos1, axis=1)          # shape: (n_envs, 3)
+        pos1 = torch.tensor(pos1, dtype=verts_batch.dtype)
         pos2 = self.bunny.get_pos()     # shape: (n_envs, 3)
         pos3 = self.cylinder.get_pos()  # shape: (n_envs, 3)
 
@@ -115,8 +150,28 @@ class Train_Env_Gathering(Train_Env):
         d23 = torch.norm(pos2 - pos3, dim=1)  # ||p2 - p3||
         d13 = torch.norm(pos1 - pos3, dim=1)  # ||p1 - p3||
 
-        rewards = -(d12 + d23 + d13)          # negative sum of pairwise distances
-        return rewards.detach().cpu().tolist()                # list[float] of length n_envs
+        # rewards = -(d12 + d23 + d13)          # negative sum of pairwise distances
+
+        rewards = []
+
+        for i in range(self.n_envs):
+            verts = verts_batch[i]  # (n_vertices, 3)
+            verts_to_pos1 = torch.norm(verts - pos1[i], dim=1)  # (n_vertices,)
+            verts_to_pos2 = torch.norm(verts - pos2[i], dim=1)
+            verts_to_pos3 = torch.norm(verts - pos3[i], dim=1)
+
+            d12_i = d12[i]
+            d23_i = d23[i]
+            d13_i = d13[i]
+
+            reward = -(d12_i + d23_i + d13_i)          # negative sum of pairwise distances
+            reward -= torch.min(verts_to_pos1) * 0.1   # negative min distance from rope to pos1
+            reward -= torch.min(verts_to_pos2) * 0.1   # negative min distance from rope to pos2
+            reward -= torch.min(verts_to_pos3) * 0.1   # negative min distance from rope to pos3
+
+            rewards.append(reward.item())
+
+        return rewards             # list[float] of length n_envs
 
     def reset(self):
         self.scene.reset()
